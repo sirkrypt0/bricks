e6d8986e3f2c9ae9c83b1e2d5b230b84e44f8427
Tobias <Tobias@DESKTOP-SJL8ELF.fritz.box>
2019-04-06 18:31:17 +0100

    Added bar movement

--- src/Bricks.java
+++ src/Bricks.java
@@ -1,9 +1,23 @@
+import java.util.concurrent.TimeUnit;
 
 public class Bricks {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		gameLoop();
+	}
+	
+	public static void gameLoop() {
+		GUI Game = new GUI();
+		while(true){
+			Game.update();
+			try {
+				TimeUnit.MILLISECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
